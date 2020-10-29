fe74cc0eb09a55172cd5273aa1dd8f6338d4a8c1
moritzsalla <moritz.salla@hotmail.de>
2020-10-29 20:20:47 +0000

    init

---
+++ src/ofApp.cpp
@@ -0,0 +1,100 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofTrueTypeFont::setGlobalDpi(72);
+    ofSetFrameRate(1000);
+    
+    string path = "/Users/moritzsalla/Library/Fonts/";
+    ofDirectory dir(path);
+    dir.allowExt("otf");
+    dir.listDir();
+    
+    string filePath;
+    fontPaths[dir.size()];
+    
+    ofLog() << dir.size();
+    
+    for(int i = 0; i < dir.size(); i++){
+        fontPaths[i] = dir.getPath(i), 14, true, true;
+    }
+    
+    font.setLineHeight(0.0f);
+    ofSetBackgroundColor(0);
+    ofSetColor(255);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    if (ofGetFrameNum() == 2805) ofExit();
+    ofSaveScreen("dollar/" + ofToString(ofGetFrameNum())+ "otf.jpg");
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofSetBackgroundColor(0);
+    ofSetColor(255);
+    
+    
+    try {
+        font.load(fontPaths[ofGetFrameNum()], 60, false);
+        font.drawString("$", 15, ofGetHeight() - 10);
+    } catch(...) {
+        ofLog() << "error loading font";
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
